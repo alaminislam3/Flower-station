747f26641d4ecce2db3a7f189ad7492e7bd07766
Alamin Islam <alamin.ndc.17@gmail.com>
2025-12-06 23:36:58 +0600

    Hero: make responsive for tablet

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import HeroTabs from "./HeroTabs";
 import { heroData } from "./HeroData";
 import logo from "../../assets/Logo/logo.png"
@@ -19,7 +19,7 @@ export default function Hero() {
 
   return (
     <div className="relative w-full h-[382px]">
-      <div className="grid grid-cols-1 lg:grid-cols-2 h-full">
+      <div className="grid grid-cols-1 md:grid-cols-2 h-full">
 
         {/* LEFT PANEL */}
         <div
@@ -28,17 +28,17 @@ export default function Hero() {
         >
           <div className="flex items-start gap-6">
             {/* Logo */}
-            <img src={logo} alt="logo" className="w-20 h-20 shrink-0" />
+            <img src={logo} alt="logo" className="w-20 h-20 shrink-0 hidden lg:block" />
 
             {/* Title, Button and Text */}
-            <div className="flex flex-col gap-4 ">
+            <div className="flex flex-col gap-4">
               {/* Title */}
-              <h1 className="text-3xl md:text-4xl font-semibold whitespace-pre-line leading-snug  flex justify-center items-center">
+              <h1 className="text-4xl font-semibold whitespace-pre-line leading-snug">
                 {slide.title}
               </h1>
 
               {/* Button */}
-             <button
+              <button
                 style={{
                   background: slide.buttonBg,
                   color: slide.buttonTextColor,
@@ -52,7 +52,7 @@ export default function Hero() {
               </button>
 
               {/* Common text */}
-              <p className="text-sm md:text-[15px] leading-6">
+              <p className="text-[15px] leading-6">
                 Order by 5pm for same day London* delivery <br />
                 or by 3pm for next day delivery UK*
               </p>
@@ -61,15 +61,15 @@ export default function Hero() {
         </div>
 
         {/* RIGHT IMAGE */}
-        <div className="relative h-[382px] overflow-hidden">
+        <div className="relative h-[382px] overflow-hidden w-full  lg:w-full">
           <img
             src={slide.image}
             alt="hero"
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover object-center"
           />
           
           {/* TABS ON IMAGE */}
-          <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
+          <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 z-10">
             <HeroTabs active={active} setActive={setActive} />
           </div>
         </div>
